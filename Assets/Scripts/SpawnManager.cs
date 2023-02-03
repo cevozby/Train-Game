5454d63712aa66cc2281aac114e4de1f6a1f47b0
flzmmc <cevozby@hotmail.com>
2023-02-03 18:14:54 +0300

    Train spawned

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] GameObject train;
+
+    bool isReady = true;
+
+    [SerializeField] float timer;
+
+    [SerializeField] string parentObjectName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        new GameObject(parentObjectName);
+        SpawnTrain();
     }
 
     // Update is called once per frame
@@ -15,4 +24,10 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
+    void SpawnTrain()
+    {
+        Instantiate(train, transform.position, Quaternion.identity, GameObject.Find(parentObjectName).transform);
+    }
+
 }
