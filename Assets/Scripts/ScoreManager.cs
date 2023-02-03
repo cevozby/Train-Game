fd2ce029716ea681a8faff4dd3a317c5f67303c7
flzmmc <cevozby@hotmail.com>
2023-02-03 22:58:33 +0300

    Score manager generated

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int matchNumber, correctNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
