f545a28f8b7de75aa15437a46d5ad5162904efae
flzmmc <cevozby@hotmail.com>
2023-02-04 22:48:37 +0300

    Score manager updated

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static int totalMatch, currentScore;
+    public static int totalMatch, currentScore, endScore;
+
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    [SerializeField] int coef;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalMatch = 0;
+        currentScore = 0;
+        endScore = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "CORRECT" + "\t\t" + currentScore.ToString() + " of " + totalMatch.ToString();
+    }
+
+    void CalculateScore()
+    {
+        endScore = currentScore * LevelManager.currentLevel * coef;
     }
 }
